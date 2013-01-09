c07f43c18ec8786506aa4a8676a13d67f5986cbb
Tyler Brock <tyler.brock@gmail.com>
2013-01-09 13:04:37 -0500

    messing around

--- mongo_hacker.js
+++ mongo_hacker.js
@@ -231,6 +231,84 @@ DBQuery.prototype.one = function(){
     return this.limit(1)[0];
 };
 
+// Aggregation Framework Helper
+DBCollection.prototype.aggregate = function( fields ){
+    return new AggHelper( this ).match( fields );
+};
+
+AggHelper = function( collection, fields ){
+    this.collection = collection;
+    this.pipeline = [];
+};
+
+AggHelper.prototype.execute = function(){
+    var res = this.collection.runCommand("aggregate", {pipeline: this.pipeline});
+    if (!res.ok) {
+        printStackTrace();
+        throw "aggregate failed: " + tojson(res);
+    }
+    return res;
+};
+
+AggHelper.prototype.shellPrint = function(){
+    return printjson(this.execute().result);
+};
+
+AggHelper.prototype.project = function( fields ){
+    if(!fields){
+        throw "project needs fields";
+    }
+    this.pipeline.push({"$project" : fields});
+    return this;
+};
+
+AggHelper.prototype.match = function( criteria ){
+    if(!criteria){
+        throw "match needs a query object";
+    }
+    this.pipeline.push({"$match" : criteria});
+    return this;
+};
+
+AggHelper.prototype.limit = function( limit ){
+    if(!limit){
+        throw "limit needs an integer indicating the max number of documents to limit";
+    }
+    this.pipeline.push({"$limit" : limit});
+    return this;
+};
+
+AggHelper.prototype.skip = function( skip ){
+    if(!skip){
+        throw "skip needs an integer indicating the number of documents to skip";
+    }
+    this.pipeline.push({"$skip" : skip});
+    return this;
+};
+
+AggHelper.prototype.unwind = function( field ){
+    if(!field){
+        throw "unwind needs a string indicating the key of an array field to unwind";
+    }
+    this.pipeline.push({"$unwind" : "$" + field});
+    return this;
+};
+
+AggHelper.prototype.group = function( group, group_expression ){
+    if(!group_expression) group_expression = {};
+    if(!group){
+        throw "group needs an grouping key";
+    }
+    group_expression._id = "$" + group;
+    this.pipeline.push({"$group" : group_expression});
+    return this;
+};
+
+AggHelper.prototype.sort = function( sort ){
+    this.pipeline.push({"$sort" : sort});
+    return this;
+};
+
 Date.prototype.tojson = function() {
 
     var UTC = Date.printAsUTC ? 'UTC' : '';
